33508989e8e0a8a3861e5b7689f185589a1460a6
Gadzbi <crg1234@gmail.com>
2023-11-14 20:35:52 +0100

    fixed nvim?

---
+++ .config/nvim/init.lua
@@ -0,0 +1,56 @@
+-- Nvim Config --
+require("options")
+require("keymaps")
+-- Plugins --
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+plugins = {
+  "folke/which-key.nvim",
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+  "folke/neodev.nvim",
+  "Mofiqul/dracula.nvim",
+  "neovim/nvim-lspconfig",
+  "simrat39/rust-tools.nvim",
+  -- Completion framework:
+   'hrsh7th/nvim-cmp',
+  -- LSP completion source:
+   'hrsh7th/cmp-nvim-lsp',
+  -- Useful completion sources:
+   'hrsh7th/cmp-nvim-lua',
+   'hrsh7th/cmp-nvim-lsp-signature-help',
+   'hrsh7th/cmp-vsnip',
+   'hrsh7th/cmp-path',                              
+   'hrsh7th/cmp-buffer',                            
+   'hrsh7th/vim-vsnip',  
+}
+require("lazy").setup(plugins)
+require("lsp")
+
+vim.cmd("colorscheme dracula")
+
+-- Rust Tools -- 
+local rt = require("rust-tools")
+
+rt.setup({
+  tools = {},
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
+
---
+++ .config/nvim/lua/keymaps.lua
@@ -0,0 +1,61 @@
+-- Better Keymaps
+local M = {}
+-- :help map-modes
+function M.map(mode, lhs, rhs)
+	vim.api.nvim_set_keymap(mode, lhs, rhs, { silent = true })
+end
+function M.noremap(mode, lhs, rhs)
+	vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true, silent = true })
+end
+function M.exprnoremap(mode, lhs, rhs)
+	vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true, silent = true, expr = true })
+end
+
+-- stylua: ignore start
+function M.nmap(lhs, rhs) M.map('n', lhs, rhs) end
+function M.nnoremap(lhs, rhs) M.noremap('n', lhs, rhs) end
+function M.vnoremap(lhs, rhs) M.noremap('v', lhs, rhs) end
+function M.xnoremap(lhs, rhs) M.noremap('x', lhs, rhs) end
+function M.inoremap(lhs, rhs) M.noremap('i', lhs, rhs) end
+function M.tnoremap(lhs, rhs) M.noremap('t', lhs, rhs) end
+function M.exprinoremap(lhs, rhs) M.exprnoremap('i', lhs, rhs) end
+function M.exprnnoremap(lhs, rhs) M.exprnoremap('n', lhs, rhs) end
+
+-- Keymapping --
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+M.nnoremap("<leader>y", '"+y')
+M.vnoremap("<leader>y", '"+y')
+M.vnoremap('J', ":m '>+1<CR>gv=gv")
+M.vnoremap('K', ":m '<-2<CR>gv=gv")
+-- Save the pasted text (if replacing with visual mode)
+M.vnoremap('p',"pgvy")
+M.vnoremap('(',"xi()<Esc>P")
+M.vnoremap(')',"xi()<Esc>P")
+M.vnoremap('{',"xi{}<Esc>P")
+M.vnoremap('}',"xi{}<Esc>P")
+M.vnoremap('[',"xi[]<Esc>P")
+M.vnoremap(']',"xi[]<Esc>P")
+
+-- Config modification
+M.nnoremap("<leader>ocf",":e C:/Users/Gadzbi/AppData/Local/nvim/init.lua <CR>")
+M.nnoremap("<leader>soc",":source C:/Users/Gadzbi/AppData/Local/nvim/init.lua <CR>")
+
+-- Spliting
+M.nnoremap("<leader>v",":vs<CR>")
+M.nnoremap("<leader>s",":split<CR>")
+M.nnoremap("<C-h>","<C-w>h")
+M.nnoremap("<C-j>","<C-w>j")
+M.nnoremap("<C-k>","<C-w>k")
+M.nnoremap("<C-l>","<C-w>l")
+
+-- Resizing
+M.nnoremap("<C-Left>",":vertical resize +3<CR>")
+M.nnoremap("<C-Right>",":vertical resize -3<CR>")
+
+-- Move commands
+M.nnoremap("<C-d>","<C-d>zz")
+M.nnoremap("<C-u>","<C-u>zz")
+M.nnoremap("<C-f>","<C-f>zz")
+M.nnoremap("<C-b>","<C-b>zz")
