0e7f989e117435b1d6aefddbeb5a232a60bb3d0c
Gadzbi <crg1234@gmail.com>
2024-03-21 18:33:28 +0100

    moved scripts

--- .config/.scripts/vim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#mkdir ~/.vim/bundle
-git clone https://github.com/morhetz/gruvbox ~/.vim/bundle/gruvbox
-git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
-#git clone https://github.com/valloric/youcompleteme ~/.vim/bundle/YouCompleteMe
-#git clone https://github.com/rdnetto/ycm-generator ~/.vim/bundle/YCM-Generator
--- link.sh
+++ link.sh
@@ -7,9 +7,6 @@ HOME_DIR='/home/gadzbi'
 echo "Installing essential packages"
 sudo apt install build-essential git fzf ripgrep xclip
 
-#mkdir $HOME_DIR/dotfiles
-#git clone --depth 1 https://github.com/gadzbi123/dotfiles $HOME/dotfiles
-
 mkdir -p $HOME_DIR/.config
 ln -s $HOME_DIR/dotfiles/.config/nvim $HOME_DIR/.config/nvim
 
