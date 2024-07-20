838026e19250e23c613b8ac303b66939314d5c25
Kacper Nitkiewicz <crg1234@gmail.com>
2024-07-20 12:32:50 +0200

    fixed lsp

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -5,35 +5,26 @@ lsp.preset("recommended")
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'gopls'},
+  ensure_installed = { 'gopls' },
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
     end,
     gopls = function()
-        require('lspconfig').gopls.setup({
-          ---
-          -- in here you can add your own
-          -- custom configuration
-          ---
-        })
-      end,
+      require('lspconfig').gopls.setup({
+        settings = {
+          gopls = {
+            gofumpt = true,
+          },
+        },
+      })
+    end,
   },
 })
 
--- lsp.configure('lua-language-server', {
---     settings = {
---         Lua = {
---             diagnostics = {
---                 globals = { 'vim' }
---             }
---         }
---     }
--- })
-
 
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_action = lsp.cmp_action()
 cmp.setup({
   window = {
@@ -55,22 +46,22 @@ cmp.setup({
     end,
   },
   sources = {
-    {name = 'nvim_lsp'},
+    { name = 'nvim_lsp' },
   },
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = 'E',
+    warn = 'W',
+    hint = 'H',
+    info = 'I'
+  }
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -82,10 +73,40 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>rs", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>rns", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+  -- format my docs
+  if client.server_capabilities.documentFormattingProvider then
+    vim.cmd([[
+            augroup formatting
+                autocmd! * <buffer>
+                autocmd BufWritePre <buffer> lua vim.lsp.buf.format()
+            augroup END
+          ]])
+  end
 end)
+
+-- Go organize Imports
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.go" },
+  callback = function()
+    local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
+    params.context = { only = { "source.organizeImports" } }
+
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    for _, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
+        else
+          vim.lsp.buf.execute_command(r.command)
+        end
+      end
+    end
+  end,
+})
 
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+  virtual_text = true
 })
