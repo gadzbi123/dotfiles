fe220da35ac431516eee4c1493e5634503584c77
Kacper Nitkiewicz <56163675+gadzbi123@users.noreply.github.com>
2024-07-25 09:07:07 +0200

    Update link to scratch

--- link.sh
+++ link.sh
@@ -8,3 +8,5 @@ sudo apt install build-essential git fzf ripgrep xclip
 mkdir -p $HOME/.config
 ln -s $HOME/dotfiles/.config/nvim $HOME/.config/nvim
 
+# Link scratch notes
+ln -s $HOME/proj/notes/scratch.nvim ~/.cache/nvim
