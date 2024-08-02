7cc00481374a3c8077a9f61362e3d50e36f3036f
gadzbi123 <crg1234@gmail.com>
2024-08-02 15:05:20 +0200

    added spelling for polish

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -5,14 +5,14 @@
 -- noselect: Do not select, force to select one from the menu
 -- shortness: avoid showing extra messages when using completion
 -- updatetime: set updatetime for CursorHold
-vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
-vim.opt.shortmess = vim.opt.shortmess + { c = true}
+vim.opt.completeopt = { 'menuone', 'noselect', 'noinsert' }
+vim.opt.shortmess = vim.opt.shortmess + { c = true }
 vim.api.nvim_set_option('updatetime', 300)
 
 -- Fixed column for diagnostics to appear
 -- Show autodiagnostic popup on cursor hover_range
--- Goto previous / next diagnostic warning / error 
--- Show inlay_hints more frequently 
+-- Goto previous / next diagnostic warning / error
+-- Show inlay_hints more frequently
 vim.cmd([[
 set signcolumn=yes
 autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
@@ -21,7 +21,7 @@ autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
 -- Options --
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.lazyredraw = true               -- Faster scrolling
+vim.opt.lazyredraw = true -- Faster scrolling
 vim.opt.scrolloff = 5
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
@@ -32,3 +32,5 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 vim.opt.timeoutlen = 3000
 vim.opt.colorcolumn = "81"
+vim.opt.spell = true
+vim.opt.spelllang = { "pl", "en" }
