d9df3c6159b1639859ca04650bbf906c39b4c09d
gadzbi123 <crg1234@gmail.com>
2025-04-02 01:58:15 +0200

    changed linking and toml i guess

--- link.sh
+++ link.sh
@@ -6,13 +6,13 @@ echo "Installing essential packages"
 sudo apt install build-essential git fzf ripgrep xclip
 
 mkdir -p $HOME/.config
-ln -s $HOME/dotfiles/.config/nvim $HOME/.config/nvim
+ln -s $HOME/dotfiles/.config/nvim $HOME/.config
 
 # Link scratch notes
 ln -s $HOME/proj/notes/scratch.nvim ~/.cache/nvim
 
 # Link alacritty
-ln -s $HOME/dotfiles/.config/alacritty $HOME/.config/alacritty
+ln -s $HOME/dotfiles/.config/alacritty $HOME/.config
 
 # Link tmux
 ln -s dotfiles/tmux/.tmux.conf ~/
