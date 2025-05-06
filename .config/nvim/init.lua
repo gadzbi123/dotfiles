d774f920fb5cea9905d6490c1ff69c7a3e124e69
gadzbi123 <crg1234@gmail.com>
2025-05-06 13:05:55 +0200

    fixed alacritty and nvim, added arch repo

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -88,6 +88,11 @@ local plugins = {
       })
     end
   },
+  {
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {}
+  },
 }
 require("lazy").setup(plugins)
 require("lsp")
